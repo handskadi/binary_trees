c20438558895cafa98670fb489bd8e07b10bb62b
Mohamed KADI <handskadi@gmail.com>
2023-11-01 15:29:06 +0000

    Task 16: Init + tested

--- binary_trees.h
+++ binary_trees.h
@@ -44,4 +44,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+size_t bt_mk_height(const binary_tree_t *tree);
+size_t bt_mk_size(const binary_tree_t *tree);
+int _mk_pow_recur(int x, int y);
 #endif
