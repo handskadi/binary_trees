1eb803b7371dc9ed07121e7f33910dc0ee3d7b33
fatimazahraelalaoui <elalaouifatimazahra02@gmail.com>
2023-11-01 13:20:20 -0700

    fr

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -3,8 +3,8 @@
 /**
  * binary_tree_sibling - finds the sibling of a node
  * @node: pointer to the node to find the sibling
- * Return: pointer to the sibling node and NULL if node is NULL 
- * NULL if node has no siblings
+ * Return: If node is NULL or there is no sibling - NULL.
+ *         Otherwise - a pointer to the sibling.
  */
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
