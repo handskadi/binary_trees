e080c4536a74ee324cdacee79d5e4a07dfd31b90
Mohamed KADI <handskadi@gmail.com>
2023-11-02 11:20:38 +0000

    Task 9: fixed & tested

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,13 +8,13 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t measure_l = 0;
-	size_t measure_r = 0;
+	size_t m_l = 0;
+	size_t m_r = 0;
 
 	if (!tree)
 		return (0);
 
-	measure_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	measure_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (measure_l > measure_r ? measure_l : measure_r);
+	m_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	m_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (m_l > m_r ? m_l : m_r);
 }
